894f4760cf8526ca0d76edd07760e9c54ac48b38
ivory <ivory@combinate.me>
2025-02-28 09:00:30 +0800

    Checkout fixes - remove duplicate cart item in order summary

--- modules/ecommerce/public/assets/scripts/cart-items-from-local-storage.js
+++ modules/ecommerce/public/assets/scripts/cart-items-from-local-storage.js
@@ -180,7 +180,7 @@ async function listCartItems(carts, productUUIDs){
             }
                 
             if(shoppingCartItemsWrap) shoppingCartHtml += createShoppingCartHtml(data, i);  
-            if(page_url !=='/shopping-cart' && orderSummaryWrap) orderSummaryHtml += createOrderSummaryHtml(data, i); 
+            //if(page_url !=='/shopping-cart' && orderSummaryWrap) orderSummaryHtml += createOrderSummaryHtml(data, i); 
         };
 
             
